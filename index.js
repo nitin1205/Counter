409219072a167200c2ad67f73fe4683568e83f58
ShadyCoder777 <99088343+ShadyCoder777@users.noreply.github.com>
2022-04-25 00:40:07 +0530

    added additional functionality and separatwd the code in two files.

--- index.js
+++ index.js
@@ -1,67 +1,23 @@
-class Timer{
-    constructor(durationInput, startButton, pauseButton, callbacks) {
-        this.durationInput = durationInput;
-        this.startButton = startButton;
-        this.pauseButton = pauseButton;
-        if (callbacks) {
-            this.onStart = callbacks.onStart;
-            this.onTick = callbacks.onTick;
-            this.onComplete = callbacks.onComplete;
-        }
-
-        this.startButton.addEventListener('click', this.start);
-        this.pauseButton.addEventListener('click', this.pause);
-    }
-
-    start = () => {
-        if (this.onStart) {
-            this.onStart();
-        }
-        this.tick();
-        this.interval = setInterval(this.tick, 1000);
-        
-    };
-
-    tick = () => {
-        if (this.timeRemaining <= 0){
-            this.pause();
-            if (this.onComplete) {
-                this.onComplete();
-            }
-        } else {
-            this.timeRemaining = this.timeRemaining - 1;
-            if (this.onTick) {
-                this.onTick();
-            }
-        }
-    };
-
-    pause = () => {
-        clearInterval(this.interval);
-    };
-
-    get timeRemaining() {
-        return parseFloat(this.durationInput.value);
-    }
-
-    set timeRemaining(time) {
-        this.durationInput.value = time;
-    }
-}
-
-
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
+const circle = document.querySelector('circle');
 
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
+
+let currentOffset = 0;
 const timer = new Timer(durationInput, startButton, pauseButton, {
-    onStart() {
-        console.log("counter starting")
+    onStart(totalDuration) {
+        duration = totalDuration;
+        console.log("counter starting");
     },
-    onTick() {
-        console.log('Timer just ticked down')
+    onTick(timeRemaining) {
+        circle.setAttribute('stroke-dashoffset',
+        perimeter * timeRemaining / duration- perimeter
+        );
     },
     onComplete() {
-        console.log("Timer is completed.")
+        console.log("Timer is completed.");
     }
 });
